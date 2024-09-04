7247f962cf2620aaa3b63aa351954102a9271900
Joel Masiaga <masiagajoel001@gmail.com>
2024-09-04 12:49:19 +0300

    New commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -19,3 +23,15 @@ class LibraryDetailView(DetailView):
     context_object_name = 'library'
 
     
+#Registration, Login, and Logout Views
+class UserRegisterView(CreateView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
