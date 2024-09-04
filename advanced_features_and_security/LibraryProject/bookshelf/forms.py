f7c229831595029fafe832f40a7dd0f65a86ef46
Joel Masiaga <masiagajoel001@gmail.com>
2024-09-04 16:37:46 +0300

    new corrction

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,18 @@
 from django import forms
+from .models import Book
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=False)
+
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100, required=True)
+    author = forms.CharField(max_length=100, required=True)
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        return title
+
+    def clean_author(self):
+        author = self.cleaned_data.get('author')
+        return author
