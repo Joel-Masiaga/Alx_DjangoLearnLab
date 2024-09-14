8b8a85a382f873935e88cd9c8fab01eaa8ee8fbe
Joel Masiaga <masiagajoel001@gmail.com>
2024-09-14 18:41:27 +0300

    new updates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+#Blog Views
+from django.shortcuts import render, redirect
 from .models import Post
 
+
+#User views
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+
+#Blog Views
 def home(request):
     return render(request, 'blog/home.html')
 
@@ -9,3 +19,23 @@ def posts(request):
         'posts' : Post.objects.all()
     }
     return render(request, 'blog/posts.html', context)
+
+
+ 
+#User Views
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+    
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
