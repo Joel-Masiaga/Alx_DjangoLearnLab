9a42b8e71873e29e70e639d0c98e7e18e7fe81a7
Joel Masiaga <masiagajoel001@gmail.com>
2024-09-14 20:38:37 +0300

    new commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'), 
 
     path('posts/', PostListView.as_view(), name='posts'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
-    path('post/new', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
