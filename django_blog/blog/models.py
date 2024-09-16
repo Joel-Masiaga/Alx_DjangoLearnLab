014fcfedcc7dca9c7baefa0bf3b5b5a41e973ac7
Joel Masiaga <masiagajoel001@gmail.com>
2024-09-16 21:55:57 +0300

    new commit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -16,6 +17,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
 
+    tags = TaggableManager()
+    
     def __str__(self):
         return self.title
     
