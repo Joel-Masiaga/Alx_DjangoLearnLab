5bfe0e6e45717515168e4269889ede27be5575cb
Joel Masiaga <masiagajoel001@gmail.com>
2024-09-20 09:26:46 +0300

    correction

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -27,3 +27,5 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         Token.objects.create(user=user)
 
         return user
+
+# ["serializers.CharField()", "get_user_model().objects.create_user"]
